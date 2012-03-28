35d9dae70f807ea8aab8c3c3ce4c306bda562ff7
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2012-03-28 07:31:02 +0700

    tambah request focus

--- src/com/stripbandunk/kontak/action/AddKontakAction.java
+++ src/com/stripbandunk/kontak/action/AddKontakAction.java
@@ -1,7 +1,10 @@
 package com.stripbandunk.kontak.action;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 import com.stripbandunk.kontak.db.KontakDatabase;
 import com.stripbandunk.kontak.model.Kontak;
@@ -15,16 +18,38 @@ public class AddKontakAction implements OnClickListener {
 		this.tambahForm = tambahForm;
 	}
 
+	private void showInvalidMessage(String message, final EditText focus) {
+		new AlertDialog.Builder(tambahForm.getActivity())
+				.setTitle("Validasi Gagal")
+				.setMessage(message)
+				.setNeutralButton("Perbaiki",
+						new DialogInterface.OnClickListener() {
+
+							public void onClick(DialogInterface dialog,
+									int which) {
+								focus.requestFocus();
+							}
+						}).show();
+	}
+
 	public void onClick(View v) {
 		Kontak kontak = new Kontak();
 		kontak.nama = tambahForm.getTextNama().getText().toString();
 		kontak.telepon = tambahForm.getTextTelepon().getText().toString();
 		kontak.email = tambahForm.getTextEmail().getText().toString();
 
-		KontakDatabase database = KontakDatabase.getInstance();
-		database.add(kontak);
+		if (kontak.nama.trim().isEmpty()) {
+			showInvalidMessage("Nama masih kosong", tambahForm.getTextNama());
+		} else if (kontak.telepon.trim().isEmpty()) {
+			showInvalidMessage("Telepon masih kosong",
+					tambahForm.getTextTelepon());
+		} else if (kontak.email.trim().isEmpty()) {
+			showInvalidMessage("Email masih kosong", tambahForm.getTextEmail());
+		} else {
+			KontakDatabase database = KontakDatabase.getInstance();
+			database.add(kontak);
 
-		tambahForm.reset();
+			tambahForm.reset();
+		}
 	}
-
 }
